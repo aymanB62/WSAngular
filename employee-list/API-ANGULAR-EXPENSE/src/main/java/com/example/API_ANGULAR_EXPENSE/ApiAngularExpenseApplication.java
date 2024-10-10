4eb2a3d23faa560c4d6034a340289f7f3d7c16b9
Ayman <oumgharayman2004@gmail.com>
2024-10-11 01:56:49 +0200

    creation employee-list

---
+++ employee-list/API-ANGULAR-EXPENSE/src/main/java/com/example/API_ANGULAR_EXPENSE/ApiAngularExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_ANGULAR_EXPENSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAngularExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAngularExpenseApplication.class, args);
+	}
+
+}
---
+++ employee-list/API-ANGULAR-EXPENSE/src/main/java/com/example/API_ANGULAR_EXPENSE/ExpenseModel/ExpenseController/Controller.java
@@ -0,0 +1,58 @@
+package com.example.API_ANGULAR_EXPENSE.ExpenseModel.ExpenseController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.API_ANGULAR_EXPENSE.ExpenseModel.Expense;
+import com.example.API_ANGULAR_EXPENSE.ExpenseServices.service;
+
+@RestController
+@RequestMapping("/api/expenses")
+@CrossOrigin(origins = "http://localhost:4200")
+public class Controller {
+	
+	private final service Service;
+	
+	@Autowired
+    public Controller(service Service) {
+        this.Service = Service;
+    }
+	
+	@GetMapping
+	public List<Expense> getAllContacts(){
+		return Service.getAllExpenses();
+	}
+	
+	@GetMapping("/{id}")
+	public Expense getContactById(@PathVariable Long id) {
+		return Service.getExpenseById(id);
+	}
+	
+	@PostMapping
+	public void addContact(@RequestBody Expense expense) {
+		Service.addExpense(expense);
+	}
+	@PutMapping("/{id}")
+    public void updateProduct(@PathVariable Long id, @RequestBody Expense expense) {
+		Expense existingProduct = Service.getExpenseById(id);
+        if (existingProduct != null) {
+        	expense.setId(id); // Ensure the correct ID is set
+        	Service.addExpense(expense);
+        }
+    }
+	
+	@DeleteMapping("/{id}")
+	public void deleteContactById(@PathVariable Long id) {
+		Service.deleteExpenseById(id);
+	}
+}
